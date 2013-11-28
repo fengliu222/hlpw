9dfc894f3ed87c5f93672ca9f798d08c337fe760
fengliu222 <444984@qq.com>
2013-11-29 00:11:59 +0800

    'database'

---
+++ activenumber.sql
@@ -0,0 +1,10 @@
+INSERT INTO `activenumber` VALUES (31, '433549279724', '2013-11-28 23:07:13', '北京欢乐谷');
+INSERT INTO `activenumber` VALUES (32, '787890205473', '2013-11-28 23:07:13', '北京欢乐谷');
+INSERT INTO `activenumber` VALUES (33, '042749385997', '2013-11-28 23:07:13', '北京欢乐谷');
+INSERT INTO `activenumber` VALUES (34, '354564767671', '2013-11-28 23:07:13', '北京欢乐谷');
+INSERT INTO `activenumber` VALUES (35, '794316619112', '2013-11-28 23:07:13', '北京欢乐谷');
+INSERT INTO `activenumber` VALUES (36, '274678493313', '2013-11-28 23:07:13', '北京欢乐谷');
+INSERT INTO `activenumber` VALUES (37, '300149013427', '2013-11-28 23:07:13', '北京欢乐谷');
+INSERT INTO `activenumber` VALUES (38, '431011446968', '2013-11-28 23:07:13', '北京欢乐谷');
+INSERT INTO `activenumber` VALUES (39, '475912438505', '2013-11-28 23:07:13', '北京欢乐谷');
+INSERT INTO `activenumber` VALUES (40, '432572479875', '2013-11-28 23:07:13', '北京欢乐谷');
---
+++ place.sql
@@ -0,0 +1,2 @@
+INSERT INTO `place` VALUES (8, '北京欢乐谷', 1);
+INSERT INTO `place` VALUES (16, '颐和园', 0);
